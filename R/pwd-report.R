d47eaec14314e7a0c1d6e1c2c6b1ca2f0698f637
Tyler Bradley <tyler.bradley@phila.gov>
2019-08-28 11:11:56 -0400

    added function to call pwd_report template

---
+++ R/pwd-report.R
@@ -0,0 +1,28 @@
+# Create functions to call PWD report templates
+
+#' Creates PWD Report
+#'
+#' @export
+pwd_report <- function(...){
+  pdf_document_format("pwd-report", ...)
+}
+
+
+pdf_document_format <- function(
+  format, template = find_resource(format, 'template.tex'), ...
+) {
+  fmt <- rmarkdown::pdf_document(..., template = template)
+  fmt$inherits <- "pdf_document"
+  fmt
+}
+
+
+find_resource <- function(template, file = 'template.tex') {
+  res <- system.file(
+    "rmarkdown", "templates", template, "resources", file, package = "rticles"
+  )
+  if (res == "") stop(
+    "Couldn't find template file ", template, "/resources/", file, call. = FALSE
+  )
+  res
+}
