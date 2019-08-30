dc36fc758943e2e7104608cfeaa68d0d231c7f07
Tyler Bradley <tyler.bradley@phila.gov>
2019-08-30 12:45:14 -0400

    modified sticker size in script

--- package_sticker.R
+++ package_sticker.R
@@ -10,11 +10,15 @@ library(magick)
 
 logo <- image_read("man/figures/water-logo-large-white.png")
 
-pkg_sticker <- sticker(logo, package = "pwdthemes", p_family = "BrixSansRegular",
-                       s_x = 1, s_y = 0.85, s_width =1.5, s_height = 0.93,
-                       p_color = "#F37720", p_size = 20,
-                       h_fill = "#0078C8", h_color = "#F37720",
-                       h_size = 2)
+# when this command is run it will overwrite the pwdthemes.png file. The two lines
+# below it must be run to correctly size the sticker.
+sticker(logo, package = "pwdthemes",
+        s_x = 1, s_y = 0.85, s_width =1.5, s_height = 0.93,
+        p_color = "#F37720", p_size = 20,
+        h_fill = "#0078C8", h_color = "#F37720",
+        h_size = 2)
 
 
-pkg_sticker
+pkg_sticker <- image_read("pwdthemes.png")
+
+pkg_sticker %>% image_scale(250) %>% image_write("pwdthemes.png")
