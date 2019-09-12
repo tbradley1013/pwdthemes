734fe48d505a54d727c47025bc2e7286b7a0a98e
Tyler Bradley <tyler.bradley@phila.gov>
2019-09-12 13:36:49 -0400

    added function to make title capitalized

--- R/theme-pwd.R
+++ R/theme-pwd.R
@@ -14,16 +14,28 @@ theme_pwd <- function(base_size = 12, base_family = "Calibri"){
       panel.grid.minor = element_blank()
     ) +
     theme(
+      # general
       text = element_text(color = "#0078C8"),
-      axis.line = element_line(color = "#001A70"),
-      axis.ticks = element_line(color = "#001A70"),
-      # axis.line = element_line(color = "#C6DAE7"),
       plot.title = element_text(color = "#0078C8", face = "bold"),
       plot.subtitle = element_text(color = "#0078C8"),
       # panel.background = element_rect(fill = "#C6DAE7"),
       panel.grid.major = element_line(color = "#C6DAE7"),
       # axis.title = element_text(color = "#0078C8"),
+      # axis
+      axis.line = element_line(color = "#001A70"),
+      axis.ticks = element_line(color = "#001A70"),
       axis.title = element_text(color = "#001A70"),
       axis.text = element_text(color = "#001A70")
     )
+
+  class(out) <- c("pwd_theme", class(out))
+
+  return(out)
+}
+
+
+ggplot_add.pwd_theme <- function(object, plot, object_name){
+  plot$theme <- ggplot2:::update_theme(plot$theme, object)
+  plot$labels$title <- stringr::str_to_upper(plot$labels$title)
+  plot
 }
