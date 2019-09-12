6451a3fceb008bb26548331d9046748a83d850a9
Tyler Bradley <tyler.bradley@phila.gov>
2019-09-12 14:17:08 -0400

    added error checking for font families

--- R/theme-pwd.R
+++ R/theme-pwd.R
@@ -3,6 +3,33 @@
 #'
 theme_pwd <- function(base_size = 12, base_family = "Calibri"){
 
+
+  if (!font_check(base_family)){
+    if (font_check("Calibri")){
+      warning(base_family, " is not an available font, switching to default Calibri", call. = FALSE)
+      base_family <- "Calibri"
+      family_light <- "Calibri Light"
+    } else if (font_check("Open Sans")){
+      base_family <- "Open Sans"
+      family_light <- "Open Sans Light"
+      warning(base_family, " is not an available font, switching to Open Sans", call. = FALSE)
+    } else if (font_check("Public Sans")){
+      base_family <- "Public Sans"
+      family_light <- "Public Sans Light"
+      warning(base_family, " is not an available font, switching to Public Sans", call. = FALSE)
+    } else {
+      base_family <- ""
+      family_light <- ""
+      warning(base_family, " is not an available font, switching to ggplot2 default", call. = FALSE)
+    }
+  } else {
+    if (font_check(paste(base_family, "Light"))){
+      family_light <- paste(base_family, "Light")
+    } else {
+      family_light <- base_family
+    }
+  }
+
   out <- theme_bw(
     base_size = base_size,
     base_family = base_family
@@ -29,8 +56,8 @@ theme_pwd <- function(base_size = 12, base_family = "Calibri"){
       # axis
       axis.line = element_line(color = "#001A70"),
       axis.ticks = element_line(color = "#001A70"),
-      axis.title = element_text(color = "#001A70", family = "Calibri Light"),
-      axis.text = element_text(color = "#001A70", family = "Calibri Light")
+      axis.title = element_text(color = "#001A70", family = family_light),
+      axis.text = element_text(color = "#001A70", family = family_light)
     )
 
   class(out) <- c("pwd_theme", class(out))
