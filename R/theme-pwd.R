f6f47a75766b88748c62847967203ce29a2fef0f
Tyler Bradley <tyler.bradley@phila.gov>
2019-09-12 15:15:51 -0400

    working on default geoms

--- R/theme-pwd.R
+++ R/theme-pwd.R
@@ -56,7 +56,10 @@ theme_pwd <- function(base_size = 12, base_family = "Calibri"){
       axis.line = element_line(color = "#001A70"),
       axis.ticks = element_line(color = "#001A70"),
       axis.title = element_text(color = "#001A70", family = family_light),
-      axis.text = element_text(color = "#001A70", family = family_light)
+      axis.text = element_text(color = "#001A70", family = family_light),
+
+      # legend
+      legend.text = element_text(color = "#001A70")
     )
 
   class(out) <- c("pwd_theme", class(out))
@@ -69,4 +72,60 @@ ggplot_add.pwd_theme <- function(object, plot, object_name){
   plot$theme <- ggplot2:::update_theme(plot$theme, object)
   plot$labels$title <- stringr::str_to_upper(plot$labels$title)
   plot
+}
+
+
+
+pwd_geoms <- function(){
+
+  # change black to resolution blue
+  replace_geom_aes_defaults("colour", "black", "#001A70")
+  replace_geom_aes_defaults("fill", "black", "#001A70")
+
+  # change grey20 to botticelli
+  replace_geom_aes_defaults("colour", "grey20", "#C6DAE7")
+  replace_geom_aes_defaults("fill", "grey20", "#C6DAE7")
+
+  # change abline color to main PWD color
+  replace_geom_aes_defaults("colour", "#3366FF", "#0078C8")
+
+  # Change geom smooth fill color
+  replace_geom_aes_defaults("fill", "grey60", "#BBDDE1")
+
+
+}
+
+undo_pwd_geoms <- function(){
+  # change resolution blue to black
+  replace_geom_aes_defaults("colour", "#001A70", "black")
+  replace_geom_aes_defaults("fill", "#001A70", "black")
+
+  # change grey20 to botticelli
+  replace_geom_aes_defaults("colour", "#C6DAE7", "grey20")
+  replace_geom_aes_defaults("fill", "#C6DAE7", "grey20")
+
+  # change abline color to main PWD color
+  replace_geom_aes_defaults("colour", "#0078C8", "#3366FF")
+
+  # change geom smooth fill back
+  replace_geom_aes_defaults("fill", "#BBDDE1", "grey60")
+}
+
+
+# These functions were used from Stack Overflow: https://stackoverflow.com/questions/21174625/ggplot-how-to-set-default-color-for-all-geoms
+# function to get the defualt geom settins for ggplot2
+geom_aes_defaults <- function() {
+  geom_names <- apropos("^Geom", ignore.case = FALSE)
+  geoms <- mget(geom_names, env = asNamespace("ggplot2"))
+  purrr::map(geoms, ~ .$default_aes)
+}
+
+# function to update ggplot2 geoms
+replace_geom_aes_defaults <- function(name, old_aes, new_aes) {
+  matching_geoms <-
+    purrr::map(geom_aes_defaults(), name) %>%
+    purrr::compact() %>%
+    purrr::keep(~ !is.na(.) & . == old_aes)
+  geoms <- gsub("^Geom(.*)", "\\1", names(matching_geoms))
+  purrr::walk(geoms, update_geom_defaults, setNames(list(new_aes), name))
 }
