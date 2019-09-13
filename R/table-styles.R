dad3206df2cbd3cebf3b26e051c1641c950ec8e1
Tyler Bradley <tyler.bradley@phila.gov>
2019-09-13 10:07:53 -0400

    adding table styling options

---
+++ R/table-styles.R
@@ -0,0 +1,25 @@
+#===============================================================================
+# Functions for table styling
+#
+# Tyler Bradley
+# 2019-09-13
+#===============================================================================
+
+
+pwd_styling <- function(kable, ...){
+  if (!inherits(kable, "knitr_kable")) stop("input must be of class `knitr_kable`")
+
+  out <- kable %>%
+    kableExtra::row_spec(0, color = "white", background = "#0078C8")
+
+
+  if (attr(kable, "format") == "latex"){
+    out <- out %>%
+      kableExtra::kable_styling(latex_options = "striped", stripe_color = "#C6DAE7", ...)
+  } else {
+    out <- out %>%
+      kableExtra::kable_styling(bootstrap_options = "striped", ...)
+  }
+
+  return(out)
+}
