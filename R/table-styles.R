e022167bea86a11b05240a0016d1699b1d1aad1c
Tyler Bradley <tyler.bradley@phila.gov>
2019-09-16 09:04:13 -0400

    working on function docs

--- R/table-styles.R
+++ R/table-styles.R
@@ -5,7 +5,15 @@
 # 2019-09-13
 #===============================================================================
 
+#' Add PWD styling to kable functions
 #'
+#' @param kable the kable that you with to style
+#' @param ... additional params to be passed to kableExtra::kable_styling
+#'
+#' @details
+#'
+#'
+#' @export
 pwd_styling <- function(kable, ...){
   if (!inherits(kable, "knitr_kable")) stop("input must be of class `knitr_kable`")
 
@@ -32,7 +40,10 @@ pwd_styling <- function(kable, ...){
 #' This function will define css in your HTML Rmarkdown document
 #' that will customize the format of kable's to meet PWD style
 #' guidelines. This function only needs to be called once at the beginning
-#' of your Rmarkdown document and will only work with html output
+#' of your Rmarkdown document and will only work with html output.
+#'
+#' The css makes the header of the table have a blue background with white
+#' letters and the striped table have a light cyan background.
 #'
 #'
 #' @export
